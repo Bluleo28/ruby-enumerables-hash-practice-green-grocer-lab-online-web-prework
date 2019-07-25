95c8e25ba386da20ce6b8bd7933867875a5f3419
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-07-25 04:30:20 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -2,12 +2,8 @@ expect (attribute.keys) .to include(:count)
 expect (attribute[:count]) .to eq(1)
 end
 expect (result)["AVOCADO"][:price]) .to eq(3.00)
-def apply_coupons(cart, coupons)
-  # code here
 end
 
-def apply_clearance(cart)
-  # code here
 end
 
 def checkout(cart, coupons)

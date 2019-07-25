10ff12b1e4b25e1b43b73d049fb2e27cd4f80202
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-07-25 04:32:22 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -5,5 +5,5 @@ expect (result)["AVOCADO"][:price]) .to eq(3.00)
 end
 expect (@avocado_result.keys) .to include("AVOCADO W/COUPON")
 end
-
+expect(@avocado_result["AVOCADO W/COUPON"][:price]) .to eq (2.50)
 end
